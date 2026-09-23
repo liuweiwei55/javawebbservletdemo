4bbb283cea2610fb6d1089a1466e3f8271347951
liuweiwei55 <2479412487@qq.com>
2026-09-23 22:23:12 +0800

    master commit3

--- src/main/java/test/demo1.java
+++ src/main/java/test/demo1.java
@@ -2,7 +2,7 @@ package test;
 
 public class demo1 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world git");
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hot-fix");
