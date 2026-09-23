3cf0879369de4304240e7034fc88e51e1b47f864
liuweiwei55 <2479412487@qq.com>
2026-09-23 22:13:13 +0800

    hot-fix commit1

--- src/main/java/test/demo1.java
+++ src/main/java/test/demo1.java
@@ -5,5 +5,6 @@ public class demo1 {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("hot-fix");
     }
 }
